8522c90be24ff9962a29d1738c3ed9d684537845
OkadaGenya <g.okada.wilf+github@gmail.com>
2019-10-22 13:37:04 +0900

    add

---
+++ src/components/ApiTest.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="ApiTest">
+    <h1>{{ msg }}</h1>
+    <button @click="request">aaaa</button>
+    <ul>
+      <li v-for="(v, index) in items" :key="v.id">
+        <a :href="v.html_url" target="_blank">
+          {{index}}
+          {{v.id}}
+          {{v.name}}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "ApiTest",
+  props: {
+    msg: String
+  },
+  data: function () {
+    return {
+      items: [],
+      res: {},
+      err: {}
+    }
+  },
+  methods: {
+    request() {
+      const successUrl = "https://api.github.com/search/repositories";
+      axios
+        .get(successUrl, {
+          params: {
+            q: "stars:>1",
+            sort: "stars",
+            order: "desc",
+            type: "Repositories"
+          }
+        })
+        .then(({data}) => {
+          this.items = data.items
+          this.res = data
+        })
+        .catch(err => {
+          this.err = err
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 0 10px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <ApiTest msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ApiTest from "@/components/ApiTest.vue";
+
+export default {
+  name: "home",
+  components: {
+    ApiTest
+  }
+};
+</script>
